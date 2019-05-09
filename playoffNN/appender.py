2d16ac1bf9c2c1db4ae1bc90f2a0da5397febe68
Ethan Bensman <ebenz99@gmail.com>
2019-05-09 19:06:46 -0400

    testing set

--- playoffNN/appender.py
+++ playoffNN/appender.py
@@ -1,14 +1,16 @@
 import os
 import numpy as np 
 
-years = ['2016','2017','2019']
+training_years = ['2016','2017','2019']
+testing_years = ['2018']
+
 
 training_data = np.ndarray(shape=(90,5), dtype = "float")
-training_labels = np.ndarray(shape=(30), dtype = "float")
+training_labels = np.ndarray(shape=(90), dtype = "float")
 
 fcounter = 0
 
-for year in years:
+for year in training_years:
 	''' code for getting wins
 	dic = {}
 	with open(year+"Wins.csv","r") as winfile:
@@ -25,16 +27,51 @@ for year in years:
 			#	exit(2)
 			#else:
 			if "*" in items[1]:
-				training_labels[idx] = np.float("1")
+				training_labels[idx+(fcounter*30)] = np.float("1")
 			else:
-				training_labels[idx] = np.float("0")
+				training_labels[idx+(fcounter*30)] = np.float("0")
 			training_data[idx+(fcounter*30)][0] = np.float(items[7])
 			training_data[idx+(fcounter*30)][1] = np.float(items[10])
 			training_data[idx+(fcounter*30)][2] = np.float(items[13])
 			training_data[idx+(fcounter*30)][3] = np.float(items[19])
 			training_data[idx+(fcounter*30)][4] = np.float(items[22])
-			print(training_data[idx])
 	fcounter+=1
 
 np.save("training_data", training_data)
 np.save("training_labels", training_labels)
+
+
+testing_data = np.ndarray(shape=(30,5), dtype = "float")
+testing_labels = np.ndarray(shape=(30), dtype = "float")
+
+fcounter = 0
+
+for year in testing_years:
+	''' code for getting wins
+	dic = {}
+	with open(year+"Wins.csv","r") as winfile:
+		for line in winfile:
+			items = line.split(",")
+			dic[items[1]] = items[3]
+	'''
+	with open(year+"Stats.csv","r") as statfile:
+		for idx,line in enumerate(statfile):
+			items = line.split(",")
+			#if items[1] not in dic:
+			#	print(items[1])
+			#	print("FAILED")
+			#	exit(2)
+			#else:
+			if "*" in items[1]:
+				testing_labels[idx+(fcounter*30)] = np.float("1")
+			else:
+				testing_labels[idx+(fcounter*30)] = np.float("0")
+			testing_data[idx+(fcounter*30)][0] = np.float(items[7])
+			testing_data[idx+(fcounter*30)][1] = np.float(items[10])
+			testing_data[idx+(fcounter*30)][2] = np.float(items[13])
+			testing_data[idx+(fcounter*30)][3] = np.float(items[19])
+			testing_data[idx+(fcounter*30)][4] = np.float(items[22])
+	fcounter+=1
+
+np.save("testing_data", testing_data)
+np.save("testing_labels", testing_labels)
